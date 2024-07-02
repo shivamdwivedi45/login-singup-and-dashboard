b8e603d1fbf03f51fddb07011cd7da5b54e3bd89
unknown <shivamdwivedi45247@gmail.com>
2024-07-02 23:52:07 +0530

    shivam

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -5,15 +6,14 @@ void main() {
   runApp(MyApp());
 }
 
-enum LoginResult { success, failure, notRegistered }
-
 enum DashboardOptions { contribute, practice, learn, interests, help, settings }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Project',
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
@@ -35,7 +35,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   _navigateToLoginScreen() async {
-    await Future.delayed(Duration(seconds: 3), () {});
+    await Future.delayed(Duration(seconds: 1), () {});
     Navigator.of(context).pushReplacement(
       MaterialPageRoute(builder: (context) => LoginScreen()),
     );
@@ -46,7 +46,7 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       body: Center(
         child: Image.asset(
-          'assets/splash_screen.jpeg',
+          'assets/splashex.jpeg',
           fit: BoxFit.cover,
           width: double.infinity,
           height: double.infinity,
@@ -75,7 +75,8 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  String _errorMessage = '';
+  String _usernameError = '';
+  String _passwordError = '';
 
   void _login() {
     String username = _usernameController.text;
@@ -86,19 +87,23 @@ class _LoginScreenState extends State<LoginScreen> {
       orElse: () => User(username: '', password: ''),
     );
 
-    if (user.username == '') {
-      setState(() {
-        _errorMessage = 'Account does not exist';
-      });
-    } else if (user.password != password) {
-      setState(() {
-        _errorMessage = 'Password is incorrect';
-      });
-    } else {
-      Navigator.of(context).push(
-        MaterialPageRoute(builder: (context) => DashboardScreen()),
-      );
-    }
+    setState(() {
+      _usernameError = '';
+      _passwordError = '';
+      if (username.isEmpty) {
+        _usernameError = 'Please enter your username';
+      } else if (password.isEmpty) {
+        _passwordError = 'Please enter your password';
+      } else if (user.username == '') {
+        _usernameError = 'Try again with a valid username';
+      } else if (user.password != password) {
+        _passwordError = 'Password is incorrect';
+      } else {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => DashboardScreen(username: username)),
+        );
+      }
+    });
   }
 
   @override
@@ -116,6 +121,7 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Spacer(),
             Text(
               "Hello!",
               style: TextStyle(
@@ -138,24 +144,33 @@ class _LoginScreenState extends State<LoginScreen> {
               hintText: 'Username',
               controller: _usernameController,
             ),
+            if (_usernameError.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Text(
+                  _usernameError,
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
             SizedBox(height: 10),
             CustomTextField(
               hintText: 'Password',
               obscureText: true,
               controller: _passwordController,
             ),
+            if (_passwordError.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Text(
+                  _passwordError,
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
             SizedBox(height: 20),
             CustomButton(
               text: 'Login',
               onPressed: _login,
             ),
-            if (_errorMessage.isNotEmpty) ...[
-              SizedBox(height: 10),
-              Text(
-                _errorMessage,
-                style: TextStyle(color: Colors.red),
-              ),
-            ],
             SizedBox(height: 20),
             TextButton(
               onPressed: () {
@@ -164,11 +179,30 @@ class _LoginScreenState extends State<LoginScreen> {
                 );
               },
               child: Text(
-                'CREATE ACCOUNT',
-                style: TextStyle(
+                'Sign Up',
+                style: TextStyle(fontSize: 20,
                   color: Colors.white,
                   decoration: TextDecoration.underline,
                 ),
+              ),
+            ),
+            Spacer(),
+            // Expandable widget here
+            Card(
+              margin: EdgeInsets.all(16.0),
+              child: ExpansionTile(
+                title: Text('User Details'),
+                children: <Widget>[
+                  ListTile(
+                    title: Text('Username: shivamdwivedi'),
+                  ),
+                  ListTile(
+                    title: Text('Email: shivamdwviedi45247@gmail.com'),
+                  ),
+                  ListTile(
+                    title: Text('Password: ******'),
+                  ),
+                ],
               ),
             ),
           ],
@@ -271,6 +305,10 @@ class _SignUpScreenState extends State<SignUpScreen> {
 }
 
 class DashboardScreen extends StatelessWidget {
+  final String username;
+
+  DashboardScreen({required this.username});
+
   void _navigateToOption(BuildContext context, DashboardOptions option) {
     switch (option) {
       case DashboardOptions.contribute:
@@ -288,107 +326,107 @@ class DashboardScreen extends StatelessWidget {
     }
   }
 
+  void _logout(BuildContext context) {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => LoginScreen()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'GEEKSFORGEEKS .',
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
+          'Dashboard',
+          style: TextStyle(color: Colors.orange, fontWeight: FontWeight.bold),
         ),
         backgroundColor: Colors.white,
-        iconTheme: IconThemeData(color: Colors.black),
+        iconTheme: IconThemeData(color: Colors.orange),
         actions: [
           IconButton(
-            icon: Icon(Icons.power_settings_new, color: Colors.green),
-            onPressed: () {
-              Navigator.pop(context);
-            },
+            icon: Icon(Icons.power_settings_new, color: Colors.red),
+            onPressed: () => _logout(context),
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
         child: Column(
           children: [
-            Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+              decoration: BoxDecoration(
+                color: Colors.orange,
+                borderRadius: BorderRadius.circular(10),
               ),
-              elevation: 5,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    Row(
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundImage: AssetImage('assets/user_avatar.png'),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        CircleAvatar(
-                          radius: 30,
-                          backgroundColor: Colors.green,
-                          child:
-                          Icon(Icons.person, size: 30, color: Colors.white),
+                        Text(
+                          username,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                        SizedBox(width: 10),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'SHIVAM DWIVEDI',
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              'Dr. A.P.J AbdUl kalam techinical university',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.grey,
-                                fontStyle: FontStyle.italic,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Spacer(),
-                      ],
-                    ),
-                    SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        CustomButton(
-                          text: 'TODO LIST',
-                          onPressed: () {},
-                          backgroundColor: Colors.green,
-                          textColor: Colors.white,
-                        ),
-                        CustomButton(
-                          text: 'EDIT PROFILE',
-                          onPressed: () {},
-                          backgroundColor: Colors.white,
-                          textColor: Colors.green,
+                        Text(
+                          'User Details',
+                          style: TextStyle(color: Colors.grey),
                         ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                  Icon(Icons.edit, color: Colors.green),
+                ],
               ),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 10),
+            Divider(),
             Expanded(
               child: GridView.count(
                 crossAxisCount: 2,
-                mainAxisSpacing: 20,
-                crossAxisSpacing: 20,
-                children: DashboardOptions.values.map((option) {
-                  return dashboardButton(
-                    context,
-                    _iconForOption(option),
-                    _labelForOption(option),
-                        () => _navigateToOption(context, option),
-                  );
-                }).toList(),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                children: [
+                  DashboardItem(
+                    icon: Icons.people,
+                    label: 'Contribute',
+                    onTap: () => _navigateToOption(context, DashboardOptions.contribute),
+                  ),
+                  DashboardItem(
+                    icon: Icons.school,
+                    label: 'Practice',
+                    onTap: () => _navigateToOption(context, DashboardOptions.practice),
+                  ),
+                  DashboardItem(
+                    icon: Icons.book,
+                    label: 'Learn',
+                    onTap: () => _navigateToOption(context, DashboardOptions.learn),
+                  ),
+                  DashboardItem(
+                    icon: Icons.star,
+                    label: 'Interests',
+                    onTap: () => _navigateToOption(context, DashboardOptions.interests),
+                  ),
+                  DashboardItem(
+                    icon: Icons.help,
+                    label: 'Help & Support',
+                    onTap: () => _navigateToOption(context, DashboardOptions.help),
+                  ),
+                  DashboardItem(
+                    icon: Icons.settings,
+                    label: 'Settings',
+                    onTap: () => _navigateToOption(context, DashboardOptions.settings),
+                  ),
+                ],
               ),
             ),
           ],
@@ -396,66 +434,32 @@ class DashboardScreen extends StatelessWidget {
       ),
     );
   }
+}
 
-  IconData _iconForOption(DashboardOptions option) {
-    switch (option) {
-      case DashboardOptions.contribute:
-        return Icons.edit;
-      case DashboardOptions.practice:
-        return Icons.code_off;
-      case DashboardOptions.learn:
-        return Icons.school;
-      case DashboardOptions.interests:
-        return Icons.list;
-      case DashboardOptions.help:
-        return Icons.help;
-      case DashboardOptions.settings:
-        return Icons.settings;
-    }
-  }
+class DashboardItem extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onTap;
 
-  String _labelForOption(DashboardOptions option) {
-    switch (option) {
-      case DashboardOptions.contribute:
-        return 'CONTRIBUTE';
-      case DashboardOptions.practice:
-        return 'PRACTICE';
-      case DashboardOptions.learn:
-        return 'LEARN';
-      case DashboardOptions.interests:
-        return 'INTERESTS';
-      case DashboardOptions.help:
-        return 'HELP';
-      case DashboardOptions.settings:
-        return 'SETTINGS';
-    }
-  }
+  DashboardItem({required this.icon, required this.label, required this.onTap});
 
-  Widget dashboardButton(BuildContext context, IconData icon, String label,
-      VoidCallback onPressed) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.green,
-        shape: RoundedRectangleBorder(
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.orange.shade200,
           borderRadius: BorderRadius.circular(10),
         ),
-        padding: EdgeInsets.symmetric(vertical: 20),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(icon, size: 40, color: Colors.green),
-          SizedBox(height: 10),
-          Text(
-            label,
-            style: TextStyle(
-              color: Colors.green,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, color: Colors.green, size: 40),
+            SizedBox(height: 10),
+            Text(label, style: TextStyle(fontSize: 16, color: Colors.white)),
+          ],
+        ),
       ),
     );
   }
@@ -463,26 +467,25 @@ class DashboardScreen extends StatelessWidget {
 
 class CustomTextField extends StatelessWidget {
   final String hintText;
-  final bool obscureText;
   final TextEditingController controller;
+  final bool obscureText;
 
-  CustomTextField(
-      {required this.hintText,
-        this.obscureText = false,
-        required this.controller});
+  CustomTextField({required this.hintText, required this.controller, this.obscureText = false});
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
       obscureText: obscureText,
+      style: TextStyle(color: Colors.white),
       decoration: InputDecoration(
         hintText: hintText,
-        fillColor: Colors.white.withOpacity(0.8),
-        filled: true,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: BorderSide.none,
+        hintStyle: TextStyle(color: Colors.white70),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.white70),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
         ),
       ),
     );
@@ -492,29 +495,25 @@ class CustomTextField extends StatelessWidget {
 class CustomButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
-  final Color? backgroundColor;
-  final Color? textColor;
 
-  CustomButton({
-    required this.text,
-    required this.onPressed,
-    this.backgroundColor,
-    this.textColor,
-  });
+  CustomButton({required this.text, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
-        backgroundColor: backgroundColor ?? Colors.white.withOpacity(0.8),
-        foregroundColor: textColor ?? Colors.black,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.orange,
+        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8),
+          borderRadius: BorderRadius.circular(25),
         ),
-        padding: EdgeInsets.symmetric(vertical: 16, horizontal: 50),
       ),
-      child: Text(text),
+      child: Text(
+        text,
+        style: TextStyle(color: Colors.orange, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
